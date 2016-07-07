1b053d34373fe8f78bd1237a7f706520e58367f9
KimDaeJin <daejinious@gmail.com>
2016-07-07 18:16:31 +0900

    우주를 건너

--- wecando/src/main/java/java76/pms/controller/ajax/InfoController.java
+++ wecando/src/main/java/java76/pms/controller/ajax/InfoController.java
@@ -1,5 +1,7 @@
 package java76.pms.controller.ajax;
 
+import java.util.HashMap;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java76.pms.domain.Member;
 import java76.pms.service.MemberService;
 
 @Controller("ajax.InfoController")
@@ -32,12 +35,21 @@ public class InfoController {
 	}
 	
 	@RequestMapping(value="member", method=RequestMethod.POST)
-	public String infoMember(String m_password,
+	public Object infoMember(String m_password,
       HttpServletResponse response, 
       HttpSession session) {
 		System.out.println(session.getAttribute("loginUser"));
+		Member member = (Member)session.getAttribute("loginUser");
 		
-		return "/info/memberInfo.html";
+		HashMap<String, Object> resultMap = new HashMap<>();
+		
+		if (!(member.getM_password().equals(m_password))) {
+			resultMap.put("status", "failure");
+			return resultMap;
+		}
+		resultMap.put("status", "success");
+		resultMap.put("loginUser", member);
+		return resultMap;
 	}
 
 }
