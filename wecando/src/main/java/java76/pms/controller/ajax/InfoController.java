6f7ad2a3584592622a99d702c3add9dddca93c66
KimDaeJin <daejinious@gmail.com>
2016-07-27 13:53:58 +0900

    .

--- wecando/src/main/java/java76/pms/controller/ajax/InfoController.java
+++ wecando/src/main/java/java76/pms/controller/ajax/InfoController.java
@@ -72,5 +72,11 @@ public class InfoController {
 		SearchSchool search = new SearchSchool();
 		return search.searchForSchool(sch_name);
 	}
+	
+	@RequestMapping(value="searchSchool", method=RequestMethod.GET)
+	public Object getSchool(String sch_name) throws Exception {
+		SearchSchool search = new SearchSchool();
+		return search.searchForSchool(sch_name);
+	}
 
 }
